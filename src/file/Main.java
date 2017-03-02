7820512bd4ad74d8ec87dd5f236b6d3444c86184
Adrian <adrpieper@gmail.com>
2017-03-02 19:27:42 +0100

    Dodałem Main próbujący otworzyć plik, który może wyrzucać wyjątek FileNotFoundException

---
+++ src/file/Main.java
@@ -0,0 +1,16 @@
+package file;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+/**
+ * Created by Adrian on 2017-03-02.
+ */
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        FileInputStream is = new FileInputStream("jakisPlik.txt");
+
+    }
+}
